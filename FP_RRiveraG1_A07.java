ad2594aa74b6cd03a87ad243b4dcd167037f9244
Riverto <a07025304@itesm.mx>
2016-09-22 23:26:49 -0500

    fix table format

--- FP_RRiveraG1_A07.java
+++ FP_RRiveraG1_A07.java
@@ -30,7 +30,7 @@ public class FP_RRiveraG1_A7 {
         for (int i=0; i<a.length; i++){
             for (int j=0; j<a[0].length;j++){
                 System.out.print(i*j+"\t");
-            } System.out.println("\n");
+            } System.out.print("\n \n \n");
         }
     }
 }
