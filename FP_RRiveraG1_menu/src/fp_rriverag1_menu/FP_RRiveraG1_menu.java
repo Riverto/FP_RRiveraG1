2899837fe1ffaec294d42035298ebf7a8552efd2
Roberto Rivera <Roberto Rivera>
2016-10-03 11:47:12 -0500

    add new project

---
+++ FP_RRiveraG1_menu/src/fp_rriverag1_menu/FP_RRiveraG1_menu.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fp_rriverag1_menu;
+
+/**
+ *
+ * @author Riverto
+ */
+public class FP_RRiveraG1_menu {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
